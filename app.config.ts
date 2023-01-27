2d23b52f80c6dc5214cdde1868872bd97a430d5d
Zack Hatlen <zckhtln@icloud.com>
2023-01-27 18:06:03 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,6 @@ export default defineAppConfig({
   docus: {
     title: "Docus",
     description: "The best place to start your documentation.",
-    image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
-    socials: {
-      twitter: "nuxtstudio",
-      github: "nuxt-themes/docus",
-      facebook: "",
-      instagram: "",
-      youtube: "",
-      medium: ""
-    },
     aside: {
       level: 0,
       collapsed: false,
@@ -28,26 +19,7 @@ export default defineAppConfig({
       fixed: true,
       fluid: true
     },
-    github: {
-      dir: ".docs/content",
-      branch: "main",
-      repo: "docus",
-      owner: "nuxt-themes",
-      edit: true
-    },
     footer: {
-      credits: {
-        icon: "IconDocus",
-        text: "Powered by Docus",
-        href: "https://docus.dev"
-      },
-      iconLinks: [
-        {
-          href: "https://nuxt.com",
-          icon: "IconNuxtLabs",
-          label: "Nuxt"
-        }
-      ],
       fluid: true
     }
   },
